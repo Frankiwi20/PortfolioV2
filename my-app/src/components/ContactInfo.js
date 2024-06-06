bd11ac6a499501fdafb7b3b152356afcf571a14a
hashe <fmurcia1@toromail.csudh.edu>
2024-06-06 01:10:02 -0700

    updated contact info

--- my-app/src/components/ContactInfo.js
+++ my-app/src/components/ContactInfo.js
@@ -3,12 +3,26 @@ import './ContactInfo.css';
 
 function ContactInfo() {
     return (
-        <section id="contact" className="section">
+        <section id="contact" className="cont-section">
             <div className="contact-container">
                 <h2>Contact</h2>
-                <p>Feel free to contact me at:</p>
-                <p>Email: <a href="mailto:fmurcia1@toromail.csudh.edu">fmurcia1@toromail.csudh.edu</a></p>
-                <p>Phone: (310) 693-3854</p>
+                <p>Feel free to contact me and check out my LinkedIn + Github pages.</p> <br/>
+                <div className="contact-item">
+                    <span>Email: </span>
+                    <a href="mailto:fmurcia1@toromail.csudh.edu">fmurcia1@toromail.csudh.edu</a>
+                </div>
+                <div className="contact-item">
+                    <span>Phone: </span>
+                    <span>(310) 693-3854</span>
+                </div>
+                <div className="contact-item">
+                    <span>LinkedIn: </span>
+                    <a href="https://www.linkedin.com/in/frankie-murcia-87b085214" target="_blank" rel="noopener noreferrer">Frankie Murcia</a>
+                </div>
+                <div className="contact-item">
+                    <span>GitHub: </span>
+                    <a href="https://github.com/Frankiwi20" target="_blank" rel="noopener noreferrer">Frankiwi20</a>
+                </div>
             </div>
         </section>
     );
